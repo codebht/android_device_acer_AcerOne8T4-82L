cefcdbbbf00d4f6e3592f66ea43376935eb9f06a
codebht <bhavitpal1412@gmail.com>
2026-09-10 14:06:14 +0530

    Update omni_AcerOne8T4-82L.mk

--- omni_AcerOne8T4-82L.mk
+++ omni_AcerOne8T4-82L.mk
@@ -5,18 +5,16 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit from those products. Most specific first.
+# Inherit from AOSP
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
-
-# Inherit from AcerOne8T4-82L device
+# Inherit from Acer One 8 T4-82L device
 $(call inherit-product, device/acer/AcerOne8T4-82L/device.mk)
 
 PRODUCT_DEVICE := AcerOne8T4-82L
-PRODUCT_NAME := omni_AcerOne8T4-82L
+PRODUCT_NAME := twrp_AcerOne8T4-82L
+
 PRODUCT_BRAND := Acer
 PRODUCT_MODEL := Acer One 8 T4-82L
 PRODUCT_MANUFACTURER := acer
